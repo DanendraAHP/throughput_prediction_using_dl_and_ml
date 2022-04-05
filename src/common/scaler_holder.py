e1ccaf9c2b9e83d36bc6a3ddc92aad04bb99b7d7
DanendraAHP <68041641+DanendraAHP@users.noreply.github.com>
2022-04-05 20:01:35 +0700

    Add files via upload

---
+++ src/common/scaler_holder.py
@@ -0,0 +1,7 @@
+from sklearn.preprocessing import RobustScaler
+from sklearn.preprocessing import StandardScaler
+
+scaler_dict = {
+    'Standard' : StandardScaler(),
+    'Robust' : RobustScaler()
+}
