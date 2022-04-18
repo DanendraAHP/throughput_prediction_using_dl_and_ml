0b7b5f7b75b2f940152de82a3b7cf4d5f8e59282
DanendraAHP <danendra6118@gmail.com>
2022-04-18 15:50:10 +0700

    fixing the sklearn model evaluation when the data is not normalized

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
 from src.pages.user_input import user_page
+from src.common.constant import PATH
+from src.common.yaml_util import read_yaml_file
 
+EXPLANATION_TEXT = read_yaml_file(PATH.config)
+EXPLANATION_TEXT = EXPLANATION_TEXT['explanation_text']
 ##############################sidebar##############################
 select_page = st.sidebar.selectbox(
     "App Navigation",
     ("Home", "Example", "Create your own")
 )
-
 ##############################main page##############################
 if select_page == "Home":
     st.title("About This App")
+    st.info(EXPLANATION_TEXT['home'])
 elif select_page=="Create your own":
     user_page()
 else:
--- test.py
+++ test.py
@@ -1,6 +1,22 @@
-from src.common.yaml_util import read_yaml_file
+import tensorflow as tf
+import numpy as np
 from src.common.constant import PATH
-
+from src.common.yaml_util import read_yaml_file
+# arr = np.random.rand(10, 20, 30)
+# # LSTM_no_ret = tf.keras.layers.LSTM(16)
+# # LSTM_ret = tf.keras.layers.LSTM(16, return_sequences=True)
+# # dense = tf.keras.layers.Dense(8)
+# print(arr.shape)
+# arr1 = tf.keras.layers.LSTM(16)(arr)
+# print(arr1.shape)
+# arr2 = tf.keras.layers.Dense(8)(arr1)
+# print(arr2.shape)
+# arr3 = tf.keras.layers.Dense(8)(arr2)
+# print(arr3.shape)
+# arr4 = tf.keras.layers.Dense(8)(arr3)
+# print(arr4.shape)
+# arr5 = tf.keras.layers.LSTM(16)(arr4)
+# print(arr5.shape)
 EXPLANATION_TEXT = read_yaml_file(PATH.config)
 EXPLANATION_TEXT = EXPLANATION_TEXT['explanation_text']
-print(EXPLANATION_TEXT['optimizer'])
+print(EXPLANATION_TEXT)
